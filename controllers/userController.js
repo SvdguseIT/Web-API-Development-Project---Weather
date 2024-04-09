2891934988184cefc389d45efbd83e1e2a34d96c
svdguseIt <svdguseit@gmail.com>
2024-04-09 21:27:00 +0530

    Rest API

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+// server/controllers/userController.js
+const User = require('../models/User');
+
+const userController = {
+  // User controller methods (e.g., register, login)
+};
+
+module.exports = userController;
---
+++ controllers/weatherController.js
@@ -0,0 +1,8 @@
+// server/controllers/weatherController.js
+const Weather = require('../server/models/Weather');
+
+const weatherController = {
+  // Weather controller methods (e.g., getAllWeatherData, getWeatherDataByDistrict, createWeatherData)
+};
+
+module.exports = weatherController;
